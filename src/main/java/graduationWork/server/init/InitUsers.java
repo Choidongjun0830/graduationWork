587b0ac3edfa6aec1003c2c1623cf13e15f34537
ChoiDongJoon <adong0808@naver.com>
2024-10-22 01:04:05 +0900

    .

--- src/main/java/graduationWork/server/init/InitUsers.java
+++ src/main/java/graduationWork/server/init/InitUsers.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class InitUsers {
 
-    private final InitUserService initUserService;
+   private final InitUserService initUserService;
 
    @PostConstruct
    public void init() {
